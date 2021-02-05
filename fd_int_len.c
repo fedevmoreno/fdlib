8c0b2e7c682b9773ab8215fdc46ef8302364bfd7
fedevmoreno <fedevmoreno@gmail.com>
2021-02-05 01:32:42 +0100

    ft_int_len.c FIX

--- fd_int_len.c
+++ fd_int_len.c
@@ -1,16 +1,16 @@
 #include "libfd.h"
 
-int			fd_ilen(int num)
-	{
-		int	len;
+int		fd_ilen(int num)
+{
+	int	len;
 
-		len = 0;
-		if (num < 0)
-			len++;
-		while (num != 0)
-		{
-			num /= 10;
-			len++;
-		}
-		return (len);
+	len = 0;
+	if (num <= 0)
+		len++;
+	while (num != 0)
+	{
+		num /= 10;
+		len++;
 	}
+	return (len);
+}
